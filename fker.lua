e9e11e22e9b11ff797b98d8ba55616ed570162ef
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2022-08-18 02:46:03 +0800

    Update fker.lua

--- fker.lua
+++ fker.lua
@@ -1,13 +1,1 @@
-local f = function(f)
-    local k, C = f[#f], ""
-    for j = 1, #k, 1 do
-        C = C .. k[f[j]]
-    end
-    return C
-end
-
-Donads = {}
-
 Donads[1] = f({3,2,1,4,5,6,7,9,8, {"a","e","D","r","R","i","d","o","h"}})
-
-say(Donads[1])
