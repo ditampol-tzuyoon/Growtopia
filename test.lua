17cab09f892be9a3a73c690852e2f4bf02f29b72
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2022-08-19 04:08:47 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -2,7 +2,7 @@
     ohdsaytag("**TRIAL SUDAH HABIS!!**\nMon Maap ya ges, Convert gih kalo ngerasa bagus :v")
     sleep(10000)
 end]]--
-
+ohdwan = true
 if ohdwan then
     ohdsay("**SC TRIAL!!**\nBerlaku Sampai Jam 12 Siang aja ya!")
     ohdwan = false
