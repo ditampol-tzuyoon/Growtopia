db0fae09062d51120a9221135f2a289b88108892
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2022-08-19 04:00:10 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,4 +1,6 @@
-while true do
+--[[while true do
     ohdsaytag("**TRIAL SUDAH HABIS!!**\nMon Maap ya ges, Convert gih kalo ngerasa bagus :v")
     sleep(10000)
-end
+end]]--
+
+ohdsay("**SC TRIAL!!**\nBerlaku Sampai Jam 12 Siang aja ya!")
