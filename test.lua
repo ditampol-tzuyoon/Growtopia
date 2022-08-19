78f94103649c22c86a1ad2848edc2088db0e88a3
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2022-08-19 17:04:04 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -2,10 +2,19 @@
     ohdsaytag("**TRIAL SUDAH HABIS!!**\nMon Maap ya ges, Convert gih kalo ngerasa bagus :v")
     sleep(10000)
 end]]--
-delayworld = 5000
 
 if ohdwan then
-    ohdsay("**SC TRIAL DIPERPANJANG!!**\nBatas Waktu Percobaan : **23.59 WITA**\n*Izin Change Delay World klean ye kujadiin 5s*")
+    if delayworld ~= 5000 then
+        ohdsay("**SC TRIAL DIPERPANJANG!!**\nBatas Waktu Percobaan : **23.59 WITA**\n*Izin Change Delay World klean ye kujadiin 5s*")
+        delayworld = 5000
+    else
+        ohdsay("**SC TRIAL DIPERPANJANG!!**\nBatas Waktu Percobaan : **23.59 WITA**)
+    end
     ohdwan = false
+end
+    
+if delayworld ~= 5000 then
+    ohdsay("**SC TRIAL DIPERPANJANG!!**\nBatas Waktu Percobaan : **23.59 WITA**\n*Izin Change Delay World klean ye kujadiin 5s*")
+    delayworld = 5000
 end
 
