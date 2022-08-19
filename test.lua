e6cd147366ac61ac4c6c12425ac987c66a9b6d30
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2022-08-19 15:26:22 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,9 +1,11 @@
---[[while true do
+while true do
     ohdsaytag("**TRIAL SUDAH HABIS!!**\nMon Maap ya ges, Convert gih kalo ngerasa bagus :v")
     sleep(10000)
-end]]--
+end
 
+--[[
 if ohdwan then
     ohdsay("**SC TRIAL!!**\nBatas Waktu Percobaan : **15:00 WITA**")
     ohdwan = false
 end
+]]--
