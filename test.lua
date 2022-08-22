865be9ba1c6cf0a0c41073497d2de39c9234c6f5
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2022-08-22 15:22:28 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -2,16 +2,6 @@
     ohdsaytag("**TRIAL SUDAH HABIS!!**\nMon Maap ya ges, Convert gih kalo ngerasa bagus :v")
     sleep(10000)
 end]]--
-
-if ohdwan then
-    if delayworld ~= 5000 then
-        ohdsay("**SC TRIAL DIPERPANJANG!!**\nBatas Waktu Percobaan : **23.59 WITA**\n*Izin Change Delay World klean ye kujadiin 5s*")
-        delayworld = 5000
-    else
-        ohdsay("**SC TRIAL DIPERPANJANG!!**\nBatas Waktu Percobaan : **23.59 WITA**")
-    end
-    ohdwan = false
-end
     
 if delayworld ~= 4000 then
     ohdsay("**SC TRIAL DIPERPANJANG!!**\nBatas Waktu Percobaan : **23.59 WITA**\n*Izin Change Delay World klean ye kujadiin 4s*")
@@ -24,5 +14,5 @@ delayplace = 100
 
 if targetinfo ~= 2 then
     targetinfo = 2 --MENGIRIM WEBHOOK SETIAP BERAPA KALI LOOP?
-    ohdsay("Sorry gw ubah Loop Webhooknya menjadi 3x Kirim!\nTile PNB juga kuubah jadi 2 Tiles!\n**~Ohdear2320**")
+    ohdsay("Sorry gw ubah Loop Webhooknya menjadi 2x Kirim!\nTile PNB juga kuubah jadi 5 Tiles!\n**~Ohdear2320**")
 end
