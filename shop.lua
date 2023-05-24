2563f4f9568ce4584de67c67c18e74254dd6c441
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2023-05-25 00:22:51 +0700

    Update shop.lua

--- shop.lua
+++ shop.lua
@@ -2,4 +2,13 @@ hargapack = 20000 -- HARGA PACK
 namapack = "world_lock_10_pack" -- NAMA PACK
 MinItemPack = 10 --APABILA JUMLAH ITEM YANG ADA DIDALAM iditempack LEBIH BESAR DARIPADA MinItemPack, MAKA BOTNYA MULAI DROP PACKNYA KE STORAGE!
 maxbuy = 5 --MAKSIMAL BOTNYA AKAN REPEAT BELI PACK JIKA GEMSNYA MASIH CUKUP!
-iditempack = {242, 6250}
+iditempack = {242}
+
+JamSkrg = 21 -- JAM DI KOTAMU SEKARANG (Format 24 Jam)
+if JamSkrg >= wkt.hour then
+    Selisih = (JamSkrg  - wkt.hour)
+else
+    Selisih = ((24 + JamSkrg)  - wkt.hour)
+end
+
+odnotice("Jam : "..JamSkrg)
