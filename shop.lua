63796f983e51c9bb986947d3db4b9a58df96e2ae
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2023-07-27 09:41:53 +0700

    Update shop.lua

--- shop.lua
+++ shop.lua
@@ -2,4 +2,3 @@ namapack = "world_lock_10_pack" -- NAMA PACK
 iditempack = {242}
 IgnoreGems_PTHT = false
 MinItemPack = 10 --APABILA JUMLAH ITEM YANG ADA DIDALAM 
-WorldSeed = {"ODSHID"} --WORLD SEED, BOTNYA 
