7c6e762d5a356058842bb489475f835421764db2
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2023-07-29 14:38:01 +0700

    Update shop.lua

--- shop.lua
+++ shop.lua
@@ -2,3 +2,4 @@ namapack = "world_lock_10_pack" -- NAMA PACK
 iditempack = {242}
 IgnoreGems_PTHT = false
 MinItemPack = 10 --APABILA JUMLAH ITEM YANG ADA DIDALAM 
+UTC_Time = 0 -- JAM DI KOTAMU SEKARANG (Format 24 Jam)
