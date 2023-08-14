aed6ec70dcfb597e64daf29edbb3e6a685d8e4bb
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2023-08-15 03:42:03 +0700

    Update shop.lua

--- shop.lua
+++ shop.lua
@@ -1,8 +1,1 @@
-hargapack = 15000 -- HARGA PACK
-namapack = "thermo_blast" -- NAMA PACK
-MinItemPack = 1 --APABILA JUMLAH ITEM YANG ADA DIDALAM 
-maxbuy = 5 --MAKSIMAL BOTNYA AKAN REPEAT BELI PACK JIKA 
-iditempack = {1402}
 UTC_Time = 0 -- JAM DI KOTAMU SEKARANG (Format 24 Jam)
-nTargetLevel = 125
-UseMethod = true
