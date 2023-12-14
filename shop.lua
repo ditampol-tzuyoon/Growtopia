f26d0e7b8dc3f0040950270f52a40042c570d0ff
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2023-12-14 23:43:59 +0800

    Update shop.lua

--- shop.lua
+++ shop.lua
@@ -1,3 +1,8 @@
 UTC_Time = 8 -- JAM DI KOTAMU SEKARANG (Format 24 Jam)
 UseMethod = true
-DelayRecon = 30000 --JEDA RECONNECT
+DelayRecon = 5 * 60000 --JEDA RECONNECT
+delayworld = 7500 -- DELAY MASUK DOOR
+delayplace = 115 -- DELAY PLACE
+delaypnb = 165 -- DELAY PNB
+delayplant = 85 -- DELAY PLANT
+delayht = 100 -- DELAY HARVEST
