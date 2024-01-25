1637c8d20089fae8ee4f1bc1d8d16266670ca13a
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2024-01-25 21:13:27 +0800

    Update shop.lua

--- shop.lua
+++ shop.lua
@@ -9,6 +9,9 @@ delayht = 65 -- DELAY HARVEST
 maxbuy = 1
 MinItemPack = 1
 setoran = 50
-iditempack = {242, 882}
+iditempack = {242, 882, 2306}
 TimeRelogBotMatung = 6
 ControlViaDC = false
+
+hargapack = 10000 -- HARGA PACK
+namapack = "party_box" -- NAMA PACK
