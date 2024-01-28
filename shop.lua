dd870fbde424c1fc45cea6b23ce89eae5a3a7476
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2024-01-29 01:15:28 +0800

    Update shop.lua

--- shop.lua
+++ shop.lua
@@ -1,5 +1,6 @@
 UTC_Time = 8 -- JAM DI KOTAMU SEKARANG (Format 24 Jam)
 UseMethod = true
+SpamText = true
 DelayRecon = 2 * 60000 --JEDA RECONNECT
 delayworld = 5000 -- DELAY MASUK DOOR
 delayplace = 115 -- DELAY PLACE
