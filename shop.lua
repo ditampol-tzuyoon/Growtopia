a1f11bf303c908a9bba80afad1937e90ebd27e25
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2024-02-02 15:40:19 +0800

    Update shop.lua

--- shop.lua
+++ shop.lua
@@ -5,8 +5,7 @@ DelayRecon = 2 * 60000 --JEDA RECONNECT
 delayworld = 5000 -- DELAY MASUK DOOR
 delayplace = 115 -- DELAY PLACE
 delaypnb = 165 -- DELAY PNB
-delayplant = 75 -- DELAY PLANT
-delayht = 85 -- DELAY HARVEST
+
 maxbuy = 5
 setoran = 50
 TimeRelogBotMatung = 6
@@ -18,3 +17,11 @@ MinItemPack = 40
 iditempack = {5764, 882, 5706, 5036, 5748, 5742, 5746, 242}
 target_block = 200
 IgnoreGems_PTHT = true
+
+if bot:getLocal().level < RarityLevel then
+  delayplant = 200 -- DELAY PLANT
+  delayht = 200 -- DELAY HARVEST
+else
+  delayplant = 75 -- DELAY PLANT
+  delayht = 85 -- DELAY HARVEST
+end
