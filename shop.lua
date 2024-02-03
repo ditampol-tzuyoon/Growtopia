9fbb07944b9f6cfd9cb04226b27d789d32b76844
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2024-02-03 23:32:35 +0800

    Update shop.lua

--- shop.lua
+++ shop.lua
@@ -6,17 +6,6 @@ delayworld = 5000 -- DELAY MASUK DOOR
 delayplace = 115 -- DELAY PLACE
 delaypnb = 165 -- DELAY PNB
 
-maxbuy = 5
-setoran = 50
-TimeRelogBotMatung = 6
-ControlViaDC = true
-
-hargapack = 10000 -- HARGA PACK
-namapack = "cny_spray" -- NAMA PACK
-MinItemPack = 1
-iditempack = {5764, 242}
-target_block = 200
-
 if block then
   if block == 4584 then
     IgnoreGems_PTHT = false
