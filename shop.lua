2dee8712cd42062fe5916092ea6b14d9174a1fd1
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2024-03-02 07:49:39 +0800

    Update shop.lua

--- shop.lua
+++ shop.lua
@@ -1,7 +1,11 @@
 UTC_Time = 8 -- JAM DI KOTAMU SEKARANG (Format 24 Jam)
 UseMethod = true
 SpamText = true
-GrindingOD = false
+if MyRdp == "Michie" then
+  GrindingOD = true
+else
+  GrindingOD = false
+end
 DelayRecon = 15000 --JEDA RECONNECT
 delayworld = 5000 -- DELAY MASUK DOOR
 delayplace = 115 -- DELAY PLACE
